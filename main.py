c2a9ffd0a9a454332a74e72f61fd05f76a32fd14
Steve <stevebotella@hotmail.fr>
2021-11-24 14:25:31 +0100

    feat: refacto with function

--- main.py
+++ main.py
@@ -2,6 +2,21 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+
+
+def askuser():
+    while True:
+        user_value = ""
+        user_value_is_int = isinstance(user_value, int)
+        try:
+            user_value = int(input("Choose a value : "))
+            user_value_is_int = isinstance(user_value, int)
+        except ValueError:
+            print("Give number")
+        if user_value_is_int:
+            break
+    return user_value
 
 
 def calculate(first_value, second_value, third_value):
@@ -18,9 +33,9 @@ def calculate(first_value, second_value, third_value):
 
 
 def main():
-    first_value = int(input("Choose first value : "))
-    second_value = int(input("Choose second value : "))
-    third_value = int(input("Choose third value : "))
+    first_value = askuser()
+    second_value = askuser()
+    third_value = askuser()
 
     print(calculate(first_value, second_value, third_value))
 
